69267e606231e7a4a0100930953839d0f0137b0a
Stephen Joe Tiamsing <stevencruz377@gmail.com>
2024-06-17 21:20:15 +0800

    Added wrapper Column for encasing all child rows

--- ph_stock_journal/lib/main.dart
+++ ph_stock_journal/lib/main.dart
@@ -8,24 +8,29 @@ void main() {
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.black,
-        body: Row(
+        body: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            const SizedBox(width: 50),
-            Image.asset(
-              stockIcon,
-              width: 50
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const SizedBox(width: 50),
+                Image.asset(
+                  stockIcon,
+                  width: 50
+                ),
+                const SizedBox(width: 10),
+                TextButton(
+                  onPressed: null,
+                  style: TextButton.styleFrom(
+                    foregroundColor: const Color.fromARGB(255, 12, 243, 4),
+                    textStyle: const TextStyle(fontSize: 28)
+                  ),
+                  child: const CustomTextStyle('PH Stock Journal')
+                ),
+              ]
             ),
-            const SizedBox(width: 10),
-            TextButton(
-              onPressed: null,
-              style: TextButton.styleFrom(
-                foregroundColor: const Color.fromARGB(255, 12, 243, 4),
-                textStyle: const TextStyle(fontSize: 28)
-              ),
-              child: const CustomTextStyle('PH Stock Journal')
-            ),
-          ]
+          ],
         ),
       ),
     ),
