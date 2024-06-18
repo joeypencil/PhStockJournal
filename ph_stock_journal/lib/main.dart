c2586ecb3ea9474c8f7ad39039e620bdbc343823
Stephen Joe Tiamsing <stevencruz377@gmail.com>
2024-06-18 20:05:48 +0800

    Add borders for visualizing containers

--- ph_stock_journal/lib/main.dart
+++ ph_stock_journal/lib/main.dart
@@ -8,29 +8,48 @@ void main() {
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.black,
-        body: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const SizedBox(width: 50),
-                Image.asset(
-                  stockIcon,
-                  width: 50
-                ),
-                const SizedBox(width: 10),
-                TextButton(
-                  onPressed: null,
-                  style: TextButton.styleFrom(
-                    foregroundColor: const Color.fromARGB(255, 12, 243, 4),
-                    textStyle: const TextStyle(fontSize: 28)
-                  ),
-                  child: const CustomTextStyle('PH Stock Journal')
-                ),
-              ]
+        body: Container(
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 4,
+              color: Colors.orange
             ),
-          ],
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Container(
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 2,
+                    color: Colors.green
+                  ),
+                  borderRadius: BorderRadius.circular(15)
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Image.asset(
+                      stockIcon,
+                      width: 50
+                    ),
+                    TextButton(
+                      onPressed: null,
+                      style: TextButton.styleFrom(
+                        foregroundColor: const Color.fromARGB(255, 12, 243, 4),
+                        textStyle: const TextStyle(fontSize: 28)
+                      ),
+                      child: const CustomTextStyle('PH Stock Journal')
+                    ),
+                  ]
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     ),
